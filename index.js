fd0ace7173fc809e625aebcf30b0d08c0e9d8b4c
Tarekul Islam <probolbd@yahoo.com>
2021-03-23 08:28:03 +0600

    update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 
 const app = express();
-
+const users = ["tarekul", "soma"];
 app.get("/", (req, res) => {
-  res.send("Thank you for calling me");
+  res.send("Thank you Tarekul calling me");
+});
+app.get("/fruit/banana", (req, res) => {
+  res.send({ fruit: "banana", quantity: 200 });
+});
+app.get("/users/:id", (req, res) => {
+  const userId = req.params.id;
+  const name = users[userId];
+  res.send(name);
 });
 app.listen(3000, () => console.log("Listening on port 3000"));

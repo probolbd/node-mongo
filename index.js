e1361c69f3bc39e25539a57fd18549b276725dd2
Tarekul Islam <probolbd@yahoo.com>
2021-03-24 23:04:11 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
 const users = ["tarekul", "soma"];
 app.get("/", (req, res) => {
   res.send("Thank you Tarekul calling me");
@@ -9,8 +15,15 @@ app.get("/fruit/banana", (req, res) => {
   res.send({ fruit: "banana", quantity: 200 });
 });
 app.get("/users/:id", (req, res) => {
-  const userId = req.params.id;
-  const name = users[userId];
-  res.send(name);
+  const id = req.params.id;
+  const name = users[id];
+  res.send({ id, name });
+});
+
+app.post("/addUser", (req, res) => {
+  //save to data base
+  const user = req.body;
+  user.id = 55;
+  res.send(user);
 });
 app.listen(3000, () => console.log("Listening on port 3000"));
